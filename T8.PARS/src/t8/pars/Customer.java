acd299592a574c3d2d8cfb71b0f40f79016de6e3
Charles Flood <charlesflood3@yahoo.com>
2012-05-05 10:23:45 -0700

    Added attributes and their accessors as described in the class diagram.

--- T8.PARS/src/t8/pars/Customer.java
+++ T8.PARS/src/t8/pars/Customer.java
@@ -1,24 +1,53 @@
 package t8.pars;
+import java.util.Date;
 public class Customer {
 
-    String getName() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    private String name;
+    private String fullAddress;
+    private int phoneNumber;
+    private Date dateOfBirth;
+    private CreditCard creditCard;
+    private Flight flight;
+    private char gender;//we assume that this will not change
+    private ConfirmationNumb confirmationNumber;
+
+    public Customer(String name, String fullAddress, int phoneNumber, Date dateOfBirth, CreditCard creditCard, Flight flight, char gender, ConfirmationNumb confirmationNumber) {
+        this.name = name;
+        this.fullAddress = fullAddress;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.creditCard = creditCard;
+        this.flight = flight;
+        this.gender = gender;
+        this.confirmationNumber = confirmationNumber;
     }
 
-    String getFullAddress() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public String getName() {
+        return name;
     }
 
-    String getPhoneNumber() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public int getPhoneNumber() {
+        return phoneNumber;
     }
 
-    String DOB() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public Date getDateOfBirth() {
+        return dateOfBirth;
     }
 
-    String getGender() {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public CreditCard getCreditCard() {
+        return creditCard;
+    }
+
+    public ConfirmationNumb getConfirmationNumber() {
+        return confirmationNumber;
+    }
+
+    public void setConfirmationNumber(ConfirmationNumb confirmationNumber) {
+        this.confirmationNumber = confirmationNumber;
+    }
+    
+    public void change(String type, String value){
+        //I'm not sure what this is supposed to do.
     }
 
 }
