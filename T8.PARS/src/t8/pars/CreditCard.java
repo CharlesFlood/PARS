d3b74ad5aef482b98226536ef4a5f51dd98a1382
Charles Flood <charlesflood3@yahoo.com>
2012-05-05 10:23:22 -0700

    Added attributes and their accessors as described in the class diagram.

--- T8.PARS/src/t8/pars/CreditCard.java
+++ T8.PARS/src/t8/pars/CreditCard.java
@@ -1,14 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package t8.pars;
 
+import java.util.Date;
 /**
  *
- * @author
+ * @author Charles Flood
  */
 public class CreditCard {
+    private Date expirationDate;
+    private String nameOnCard;
+    private int cardNumber;
+    private int securityNumber;
 
+    public CreditCard(Date expirationDate, String nameOnCard, int cardNumber, int securityNumber) {
+        this.expirationDate = expirationDate;
+        this.nameOnCard = nameOnCard;
+        this.cardNumber = cardNumber;
+        this.securityNumber = securityNumber;
+    }
+    public int getCardNumber() {
+        return cardNumber;
+    }
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+    public String getNameOnCard() {
+        return nameOnCard;
+    }
+    public int getSecurityNumber() {
+        return securityNumber;
+    }
 }
